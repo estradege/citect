3f9fb780992f8f0ef122b6230e44e4cf66128646
Geoffrey Estrade <geoffrey.estrade@amics.ch>
2020-03-01 17:55:36 +0100

    version

---
+++ Citect.CtApi/Citect.CtApi.Samples.CiCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Citect.CtApi.Samples.CiCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Citect!");
+            try
+            {
+                using (var ctApi = new CtApi())
+                {
+                    ctApi.Open();
+                    var result = ctApi.Cicode("PageDisplay(Alarm)");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Citect.CtApi/Citect.CtApi.Samples.Find/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Citect.CtApi.Samples.Find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Citect!");
+            try
+            {
+                using (var ctApi = new CtApi())
+                {
+                    ctApi.Open();
+                    var alarms = ctApi.Find("Alarm", "TAG=BP12*", "", "TAG", "NAME", "DESC");
+
+                    foreach (var alarm in alarms)
+                    {
+                        Console.WriteLine($"TAG={alarm["TAG"]}, NAME={alarm["NAME"]}, DESC={alarm["DESC"]}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
