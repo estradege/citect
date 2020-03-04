62d4ab92ff18873d90de39ed2045404a6f05f430
Geoffrey Estrade <geoffrey.estrade@amics.ch>
2020-03-04 23:58:08 +0100

    cleanup

--- Citect.AlarmDriver/Citect.AlarmDriver.ConsoleApp1/Program.cs
+++ Citect.AlarmDriver/Citect.AlarmDriver.ConsoleApp1/Program.cs
@@ -10,50 +10,15 @@ namespace Citect.AlarmDriver.ConsoleApp1
         {
             Console.WriteLine("Hello Citect!");
 
-            //using (var db = new AlarmDbConnection("AlarmServer1", "127.0.0.1", 5482))
-            //{
-            //    var result = db.Query("SELECT * FROM CiAlarmObject");
-            //    var result2 = db.QueryAsync("SELECT * FROM CiAlarmObject").Result;
-            //}
+            using (var db = new AlarmDbConnection("AlarmServer1", "127.0.0.1", 5482))
+            {
+                var result = db.Query("SELECT * FROM CiAlarmObject");
+                var result2 = db.QueryAsync("SELECT * FROM CiAlarmObject").Result;
+            }
 
             var service = new AlarmDbService("AlarmServer1", "127.0.0.1", 5482);
-            var alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-            alarmsState = service.GetLastAlarmsAsync().Result;
-
             var alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-            alarms = service.GetAlarmsAsync().Result;
-
-            var al = alarms.Where(a => a.OnTime.Year == 2020);
+            var alarmsState = service.GetLastAlarmsAsync().Result;
         }
     }
 }
-
-
-// var result = await odbc.QueryAsync($"select Equipment,AlarmSource,AckTime,OnTime,OffTime,DisableTime,AlarmState from CiAlarmObject where ({alarmCategoryFilter}){lastUpdateTimeFilter}");
-
-/*
- * Value = new AlarmValue
-    {
-        Ack = x.AlarmState == 0 || x.AlarmState == 2,
-        AckTime = ConvertFromSqlTimestamp(x.AckTime),
-        Dis = x.AlarmState == 1,
-        DisTime = ConvertFromSqlTimestamp(x.DisableTime),
-        OffTime = ConvertFromSqlTimestamp(x.OffTime),
-        On = x.AlarmState == 2 || x.AlarmState == 4,
-        OnTime = ConvertFromSqlTimestamp(x.OnTime),
-    }
- */
