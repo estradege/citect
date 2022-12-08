36f5944662309564a745d2047aef5c8d3f2dc708
Geoffrey Estrade <geoffrey.estrade@amics.ch>
2022-12-08 13:59:14 +0100

    .

--- Citect.CtApi/Citect.CtApi.ConsoleApp1/Program.cs
+++ Citect.CtApi/Citect.CtApi.ConsoleApp1/Program.cs
@@ -1,27 +1,18 @@
 ﻿using Citect;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            //var trnQueryDisplayMode = DisplayMode.Get(Ordering.OldestToNewest, Condense.Mean, Stretch.Raw, 0, BadQuality.Zero, Raw.Totaly);
-            //var starttime = DateTime.UtcNow.AddMinutes(-10);
-            //var endtime = DateTime.UtcNow.AddMinutes(-5);
-
             try
             {
-                //using (var ctApi = new CtApi(true, "", "", ""))
-                using (var ctApi = new CtApi(true, "sligo", "amics", "bison"))
-                {
-                    Console.WriteLine("connected");
-                }
+                var ctApi = new CtApi();
+                ctApi.Open();
+                Console.WriteLine("connected");
+                ctApi.Close();
             }
             catch (Exception e)
             {
---
+++ Citect.CtApi/Citect.CtApi.ConsoleApp48/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp48
+{
+    internal class Program
+    {
+        [DllImport("CtApi.dll", EntryPoint = "ctOpen", SetLastError = true)]
+        private static extern IntPtr CtOpen(string sComputer, string sUser, string sPassword, uint nMode);
+
+        [DllImport("CtApi.dll", EntryPoint = "ctClose", SetLastError = true)]
+        private static extern bool CtClose(IntPtr hCTAPI);
+
+        [DllImport("CtApi.dll", EntryPoint = "ctSetManagedBinDirectory", SetLastError = true)]
+        private static extern bool CtSetManagedBinDirectory(string sPath);
+
+        static void Main(string[] args)
+        {
+            var ctApi = IntPtr.Zero;
+
+            try
+            {
+                var result = CtSetManagedBinDirectory("C:\\ProgramData\\CitectCtApi");
+                ctApi = CtOpen(null, null, null, 0);
+                Console.WriteLine("Opened");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+                var result = CtClose(ctApi);
+                Console.WriteLine("Closed");
+            }
+        }
+    }
+}
